58ebb0ae40a265c6da1a5a84576f17769ab4f4fe
Yasvanthkumar <Yasvanthkumar@YASH>
2023-05-08 15:03:49 +0530

    Initial Commit

---
+++ ELibrary/src/main/java/servlet/DeleteLibrarian.java
@@ -0,0 +1,27 @@
+package servlet;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.LibrarianDao;
+
+@WebServlet("/DeleteLibrarian")
+public class DeleteLibrarian extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sid=request.getParameter("id");
+		int id=Integer.parseInt(sid);
+		LibrarianDao.delete(id);
+		response.sendRedirect("ViewLibrarian");
+	}
+}
